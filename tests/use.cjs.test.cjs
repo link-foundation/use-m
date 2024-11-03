212a77d50ca51c252cef8ca77e73d4685eeac5ee
Konstantin Diachenko <drakonard@gmail.com>
2024-11-04 02:48:57 +0530

    Use fetch in both tests

--- tests/use.cjs.test.cjs
+++ tests/use.cjs.test.cjs
@@ -8,11 +8,11 @@ const path = require("path");
     // const use = await fs.readFile('/Users/konard/Desktop/konard/use/src/use.cjs', 'utf8')
     //   .then((code) => eval(code));
 
-    const { use } = require('../src/use-module.cjs');
+    // const { use } = require('../src/use-module.cjs');
 
-    // const use = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use.cjs')
-    // .then((response) => response.text())
-    // .then((code) => eval(code));
+    const use = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use.cjs')
+      .then((response) => response.text())
+      .then((code) => eval(code));
 
     const _ = await use("lodash@4.17.21");
     const resultChunk = _.chunk([1, 2, 3, 4, 5], 2);
