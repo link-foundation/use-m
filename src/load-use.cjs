813dfce93c8acc6a3bd944cfbe09c7aaadabb062
Konstantin Diachenko <drakonard@gmail.com>
2024-11-04 05:45:30 +0530

    Fix load scripts

--- src/load-use.cjs
+++ src/load-use.cjs
@@ -3,10 +3,14 @@ async () => {
   const { tmpdir } = require('os');
   const { randomBytes } = require('crypto');
   const { writeFile, unlink } = require('fs/promises');
-  const response = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use-module.cjs');
-  const localUsePath = join(tmpdir(), randomBytes(42).toString('hex') + '.cjs');
-  await writeFile(localUsePath, await response.text());
-  const { use } = require(localUsePath);
-  await unlink(localUsePath);
+  const moduleDirecotry = join(tmpdir(), randomBytes(42).toString('hex'));
+  const functionResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use.cjs');
+  const functionPath = join(moduleDirecotry, 'use.cjs');
+  await writeFile(functionPath, await functionResponse.text());
+  const moduleResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use-module.cjs');
+  const modulePath = join(moduleDirecotry, 'use-module.cjs');
+  await writeFile(modulePath, await moduleResponse.text());
+  const { use } = require(modulePath);
+  await unlink(modulePath);
   return use;
 }
--- src/load-use.mjs
+++ src/load-use.mjs
@@ -3,10 +3,14 @@ async () => {
   const { tmpdir } = await import('os');
   const { randomBytes } = await import('crypto');
   const { writeFile, unlink } = await import('fs/promises');
-  const response = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use-module.mjs');
-  const localUsePath = join(tmpdir(), randomBytes(42).toString('hex') + '.mjs');
-  await writeFile(localUsePath, await response.text());
-  const { use } = await import(localUsePath);
-  await unlink(localUsePath);
+  const moduleDirecotry = join(tmpdir(), randomBytes(42).toString('hex'));
+  const functionResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use.mjs');
+  const functionPath = join(moduleDirecotry, 'use.mjs');
+  await writeFile(functionPath, await functionResponse.text());
+  const moduleResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use-module.mjs');
+  const modulePath = join(moduleDirecotry, 'use-module.mjs');
+  await writeFile(modulePath, await moduleResponse.text());
+  const { use } = require(modulePath);
+  await unlink(modulePath);
   return use;
 }
