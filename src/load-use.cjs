f5e1cf34d899a600da48c8f8e6df52def94b11b8
Konstantin Diachenko <drakonard@gmail.com>
2024-11-04 05:48:45 +0530

    Fix load-use.mjs

--- src/load-use.cjs
+++ src/load-use.cjs
@@ -3,12 +3,12 @@ async () => {
   const { tmpdir } = require('os');
   const { randomBytes } = require('crypto');
   const { writeFile, unlink } = require('fs/promises');
-  const moduleDirecotry = join(tmpdir(), randomBytes(42).toString('hex'));
+  const moduleDirectory = join(tmpdir(), randomBytes(42).toString('hex'));
   const functionResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use.cjs');
-  const functionPath = join(moduleDirecotry, 'use.cjs');
+  const functionPath = join(moduleDirectory, 'use.cjs');
   await writeFile(functionPath, await functionResponse.text());
   const moduleResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use-module.cjs');
-  const modulePath = join(moduleDirecotry, 'use-module.cjs');
+  const modulePath = join(moduleDirectory, 'use-module.cjs');
   await writeFile(modulePath, await moduleResponse.text());
   const { use } = require(modulePath);
   await unlink(modulePath);
--- src/load-use.mjs
+++ src/load-use.mjs
@@ -3,14 +3,14 @@ async () => {
   const { tmpdir } = await import('os');
   const { randomBytes } = await import('crypto');
   const { writeFile, unlink } = await import('fs/promises');
-  const moduleDirecotry = join(tmpdir(), randomBytes(42).toString('hex'));
+  const moduleDirectory = join(tmpdir(), randomBytes(42).toString('hex'));
   const functionResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use.mjs');
-  const functionPath = join(moduleDirecotry, 'use.mjs');
+  const functionPath = join(moduleDirectory, 'use.mjs');
   await writeFile(functionPath, await functionResponse.text());
   const moduleResponse = await fetch('https://raw.githubusercontent.com/Konard/use/refs/heads/main/src/use-module.mjs');
-  const modulePath = join(moduleDirecotry, 'use-module.mjs');
+  const modulePath = join(moduleDirectory, 'use-module.mjs');
   await writeFile(modulePath, await moduleResponse.text());
-  const { use } = require(modulePath);
+  const { use } = await import(modulePath);
   await unlink(modulePath);
   return use;
 }
