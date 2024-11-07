105862a1bb41cd802eb49df77c6334e87682dca5
Konstantin Diachenko <drakonard@gmail.com>
2024-11-08 01:39:02 +0530

    Refactor load-* files

--- src/load-use.cjs
+++ src/load-use.cjs
@@ -5,12 +5,18 @@ async () => {
   const { writeFile, rm, mkdir } = require('fs/promises');
   const moduleDirectory = join(tmpdir(), randomBytes(42).toString('hex'));
   await mkdir(moduleDirectory);
-  const functionResponse = await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use.cjs');
-  const functionPath = join(moduleDirectory, 'use.cjs');
-  await writeFile(functionPath, await functionResponse.text());
-  const moduleResponse = await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use-module.cjs');
-  const modulePath = join(moduleDirectory, 'use-module.cjs');
-  await writeFile(modulePath, await moduleResponse.text());
+  await writeFile(
+    join(moduleDirectory, 'use.cjs'), 
+    await (
+      await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use.cjs')
+    ).text()
+  );
+  await writeFile(
+    join(moduleDirectory, 'use-module.cjs'), 
+    await (
+      await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use-module.cjs')
+    ).text()
+  );
   const { use } = require(modulePath);
   await rm(moduleDirectory, { recursive: true, force: true });
   return use;
--- src/load-use.mjs
+++ src/load-use.mjs
@@ -5,12 +5,18 @@ async () => {
   const { writeFile, rm, mkdir } = await import('fs/promises');
   const moduleDirectory = join(tmpdir(), randomBytes(42).toString('hex'));
   await mkdir(moduleDirectory);
-  const functionResponse = await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use.mjs');
-  const functionPath = join(moduleDirectory, 'use.mjs');
-  await writeFile(functionPath, await functionResponse.text());
-  const moduleResponse = await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use-module.mjs');
-  const modulePath = join(moduleDirectory, 'use-module.mjs');
-  await writeFile(modulePath, await moduleResponse.text());
+  await writeFile(
+    join(moduleDirectory, 'use.mjs'), 
+    await (
+      await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use.mjs')
+    ).text()
+  );
+  await writeFile(
+    join(moduleDirectory, 'use-module.mjs'), 
+    await (
+      await fetch('https://raw.githubusercontent.com/link-foundation/use-m/refs/heads/main/src/use-module.mjs')
+    ).text()
+  );
   const { use } = await import(modulePath);
   await rm(moduleDirectory, { recursive: true, force: true });
   return use;
