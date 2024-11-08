da315ddd9529d2c702576f44ca525af01e0802d9
Konstantin Diachenko <drakonard@gmail.com>
2024-11-08 11:12:53 +0530

    Add example for alternative options

--- examples/execa/example.mjs
+++ examples/execa/example.mjs
@@ -1,13 +1,17 @@
-  const use = await fetch('https://unpkg.com/use-m/src/use.js')
-    .then(response => response.text())
-    .then(code => eval(code)({
-      metaUrl: import.meta.url,
-    }));
+// import { createRequire } from 'module';
 
-  const _ = await use('lodash@latest');
-  const { $: $$ } = await use('execa@latest');
-  const $ = $$({ verbose: 'full' });
+const use = await fetch('https://unpkg.com/use-m/src/use.js')
+  .then(response => response.text())
+  .then(code => eval(code)({
+    metaUrl: import.meta.url,
+    // baseResolver: createRequire(import.meta.url).resolve,
+    // baseResolver: import.meta.resolve,
+  }));
 
-  const { stdout } = await $`ls`.pipe`grep js`;
-  const files = _.filter(_.split(stdout, '\n'), (item) => !_.isEmpty(item));
-  console.log(files);
+const _ = await use('lodash@latest');
+const { $: $$ } = await use('execa@latest');
+const $ = $$({ verbose: 'full' });
+
+const { stdout } = await $`ls`.pipe`grep js`;
+const files = _.filter(_.split(stdout, '\n'), (item) => !_.isEmpty(item));
+console.log(files);
