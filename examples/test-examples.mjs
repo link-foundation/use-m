b83db7b6cf241a92511fc07e6143ef8736e60d69
Konstantin Diachenko <drakonard@gmail.com>
2024-11-09 18:51:05 +0530

    Enable autoupdate of examples

--- examples/test-examples.mjs
+++ examples/test-examples.mjs
@@ -4,7 +4,9 @@ await $`./examples/zx/example.mjs`;
 await $`node ./examples/execa/example.mjs`;
 
 cd('./examples/cjs');
+await $`npm i use-m@latest`;
 await $`npm run start`;
 
 cd('../mjs');
+await $`yarn add use-m@latest`;
 await $`yarn start`;
