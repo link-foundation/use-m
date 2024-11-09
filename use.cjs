9ea0680502fc505600bf16230e7ef6d6e3ca7378
Konstantin Diachenko <drakonard@gmail.com>
2024-11-09 19:30:02 +0530

    Sync implementations

--- use.cjs
+++ use.cjs
@@ -88,14 +88,20 @@ const resolvers = {
   jsdelivr: async (moduleSpecifier, pathResolver) => {
     const { packageName, version, modulePath } = parseModuleSpecifier(moduleSpecifier);
     // If no modulePath is provided, append /{packageName}.js
-    const path = modulePath ? `${modulePath}.js` : `/${packageName}.js`;
+    let path = modulePath ? modulePath : `/${packageName}`;
+    if (/\.(mc)?js$/.test(path) === false) {
+      path += '.js';
+    }
     const resolvedPath = `https://cdn.jsdelivr.net/npm/${packageName}-es@${version}${path}`;
     return resolvedPath;
   },
   unpkg: async (moduleSpecifier, pathResolver) => {
     const { packageName, version, modulePath } = parseModuleSpecifier(moduleSpecifier);
     // If no modulePath is provided, append /{packageName}.js
-    const path = modulePath ? `${modulePath}.js` : `/${packageName}.js`;
+    let path = modulePath ? modulePath : `/${packageName}`;
+    if (/\.(mc)?js$/.test(path) === false) {
+      path += '.js';
+    }
     const resolvedPath = `https://unpkg.com/${packageName}-es@${version}${path}`;
     return resolvedPath;
   },
@@ -104,7 +110,11 @@ const resolvers = {
     return resolvedPath;
   },
   jspm: async (moduleSpecifier, pathResolver) => {
-    const resolvedPath = `https://jspm.dev/${moduleSpecifier}`;
+    let { packageName, version, modulePath } = parseModuleSpecifier(moduleSpecifier);
+    if (version === 'latest') {
+      version = '';
+    }
+    const resolvedPath = `https://jspm.dev/${packageName}${version ? `@${version}` : ''}${modulePath}`;
     return resolvedPath;
   },
 }
