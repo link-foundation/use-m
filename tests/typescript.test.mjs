e623c4a3847890c60872232dc33cf17e09429d22
Konstantin Diachenko <drakonard@gmail.com>
2024-11-10 04:34:13 +0530

    Add tests and example for typescript import

---
+++ tests/typescript.test.mjs
@@ -0,0 +1,11 @@
+import { use } from 'use-m';
+
+describe(`typescript`, () => {
+  test('use typescript', async () => {
+    const ts = await use('typescript');
+    expect(ts.version).toBeDefined();
+    const tsCode = 'const a: number = 1;';
+    const jsCode = ts.transpile(tsCode);
+    expect(jsCode).toBe('var a = 1;\n');
+  });
+});
