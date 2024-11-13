ad4e9fbbc018ddf7a40cb549249ceeed0562a1c7
Konstantin Diachenko <drakonard@gmail.com>
2024-11-14 00:42:58 +0530

    Add tests for --experimental-network-imports support

--- tests/use.test.cjs
+++ tests/use.test.cjs
@@ -54,4 +54,13 @@ describe(`'use' import strategies (CJS)`, () => {
         expect(_.add(1, 2)).toBe(3);
       });
   });
+
+  test('Import using --experimental-network-imports', async () => {
+    const { exec } = await import('child_process');
+    const { promisify } = await import('util');
+    const execAsync = promisify(exec);
+    const { stdout: sumOf1And2 } = await execAsync('node --experimental-network-imports ./examples/network-imports/index.mjs');
+    const cleanResult = sumOf1And2.trim().replace(/\x1b\[[0-9;]*m/g, ''); // Remove ANSI color codes
+    expect(cleanResult).toEqual("_.add(1, 2) = 3");
+  });
 });
