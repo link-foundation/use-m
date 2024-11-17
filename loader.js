83bb595b84143406a155046403934671cbdf3000
Konstantin Diachenko <drakonard@gmail.com>
2024-11-18 00:12:38 +0530

    Fix bug

--- loader.js
+++ loader.js
@@ -2,8 +2,8 @@
 
 // import { resolve, extname } from 'path';
 import { resolvers } from './use.mjs';
-import { createRequire } from 'module';
-const { resolve } = createRequire(import.meta.url);
+// import { createRequire } from 'module';
+// const { resolve } = createRequire(import.meta.url);
 
 // The `resolve` hook customizes how module URLs are resolved
 export async function resolve(specifier, context, defaultResolve) {
