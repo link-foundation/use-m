8c274ee27ab1e5bf153029b611cca48fbb5550f0
Konstantin Diachenko <drakonard@gmail.com>
2024-11-17 23:48:55 +0530

    Use path relative to cli.mjs

--- cli.mjs
+++ cli.mjs
@@ -1,63 +1,54 @@
 #!/usr/bin/env node
 
-import { resolve } from 'path';
-import { execSync } from 'child_process';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { use } from 'use-m';
-
-// import yargs from 'yargs';
-// import { hideBin } from 'yargs/helpers';
 import { readFileSync } from 'fs';
 
+// Import yargs and helpers dynamically using `use`
 const yargs = await use('yargs@17.7.2');
 const { hideBin } = await use('yargs@17.7.2/helpers');
 
-// Function to get the global installation path of a package
-function getGlobalInstallPath(packageName) {
+const currentScriptDir = dirname(fileURLToPath(import.meta.url));
+
+// Function to get the loader path
+function getLoaderPath() {
+  // Get the directory of the current file (cli.mjs)
   try {
-    const path = execSync(`npm root -g`, { encoding: 'utf8' }).trim();
-    return resolve(path, packageName);
+    return resolve(currentScriptDir, 'loader.js');
   } catch (err) {
-    console.error(`Failed to determine global path for ${packageName}`);
-    process.exit(1);
+    throw new Error('Failed to get the loader path.', { cause: err });
   }
 }
 
-// Get the version from package.json
+// Function to get the version from package.json
 function getVersion() {
   try {
-    const packageJsonPath = resolve(process.cwd(), 'package.json');
+    const packageJsonPath = resolve(currentScriptDir, 'package.json');
     const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
     return packageJson.version;
   } catch (err) {
-    console.error('Failed to read package.json or determine the version.');
-    process.exit(1);
+    throw new Error('Failed to read package.json or determine the version.', { cause: err });
   }
 }
 
-// Main script logic
-function main() {
-  yargs(hideBin(process.argv))
-    .scriptName('use-m')
-    .usage('$0 [options]')
-    .option('loader-path', {
-      alias: 'l',
-      type: 'boolean',
-      description: 'Output the path to loader.js file',
-    })
-    .help('help')
-    .alias('help', 'h')
-    .version(getVersion()) // Use yargs' built-in version functionality
-    .alias('version', 'v')
-    .command('$0', 'Default command', () => {}, (argv) => {
-      if (argv.loaderPath) {
-        const loaderPath = resolve(getGlobalInstallPath('use-m'), 'loader.js');
-        console.log(loaderPath);
-      } else {
-        yargs.showHelp();
-      }
-    })
-    .parse();
-}
-
-// Execute the script
-main();
+yargs(hideBin(process.argv))
+  .scriptName('use-m')
+  .usage('$0 [options]')
+  .option('loader-path', {
+    alias: 'lp',
+    type: 'boolean',
+    description: 'Output the path to loader.js file',
+  })
+  .help('help')
+  .alias('help', 'h')
+  .version(getVersion()) // Use yargs' built-in version functionality
+  .alias('version', 'v')
+  .command('$0', 'Default command', () => { }, (argv) => {
+    if (argv.loaderPath) {
+      console.log(getLoaderPath());
+    } else {
+      yargs.showHelp();
+    }
+  })
+  .parse();
