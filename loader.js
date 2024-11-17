108eeb19b83e6caa5e7aca877c4a1d4983a88cee
Konstantin Diachenko <drakonard@gmail.com>
2024-11-18 00:19:38 +0530

    Use require.resolve to fix stack overflow

--- loader.js
+++ loader.js
@@ -2,8 +2,8 @@
 
 // import { resolve, extname } from 'path';
 import { resolvers } from './use.mjs';
-// import { createRequire } from 'module';
-// const { resolve } = createRequire(import.meta.url);
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 // The `resolve` hook customizes how module URLs are resolved
 export async function resolve(specifier, context, defaultResolve) {
@@ -20,7 +20,7 @@ export async function resolve(specifier, context, defaultResolve) {
 
   try {
     // Resolve the specifier using the npm resolver
-    const resolvedUrl = await npm(specifier, defaultResolve);
+    const resolvedUrl = await npm(specifier, require.resolve);
     return { url: resolvedUrl };
   } catch (err) {
     // // Handle errors
