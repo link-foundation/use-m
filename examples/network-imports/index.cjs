a26447515e4f62b1df961d78f5af2e896b9ff3c9
Konard <drakonard@gmail.com>
2025-08-18 06:23:34 +0530

    Temporary disable tests

--- examples/network-imports/index.cjs
+++ examples/network-imports/index.cjs
@@ -1,4 +1,4 @@
-import("https://unpkg.com/use-m/use.mjs")
+import("../../use.mjs") // TODO: replace with import("https://unpkg.com/use-m/use.mjs")
   .then(async ({ use }) => {
     const _ = await use("lodash@4.17.21");
     console.log(`_.add(1, 2) = ${_.add(1, 2)}`);
--- examples/network-imports/index.mjs
+++ examples/network-imports/index.mjs
@@ -1,4 +1,4 @@
-const { use } = await import('https://unpkg.com/use-m/use.mjs');
+const { use } = await import("https://unpkg.com/use-m/use.mjs"); // TODO: replace with import("https://unpkg.com/use-m/use.mjs")
 // const { use } = await import('https://esm.sh/use-m@8.8.0');
 const _ = await use('lodash@4.17.21');
 console.log(`_.add(1, 2) = ${_.add(1, 2)}`);
